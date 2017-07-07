c9af4a25256bfa8785b5f7f2cb0d8b9bd8e48916
srdjan034 <snikolic.imi@gmail.com>
2017-07-07 19:56:38 +0200

    Update Particle.py
    Izbacena cestica kod racunanja gausove raspodele.

--- rabbitMQ/Particle.py
+++ rabbitMQ/Particle.py
@@ -1,5 +1,5 @@
-from random import random, seed
 from math import *
+from random import random, seed
 
 # Konstante
 INIT=-1
@@ -8,43 +8,14 @@ WALL=1
 TOP=2
 BOTTOM=3
 
+gauss_kon = 0
+gauss_y = 0.0
+
 # Parametri posla
 r = 0.04
 h = 0.08
 half_path = 6.0e-6
 tau = 183/log(2)
-
-
-# Stampaj status
-def print_status(nAir, nWall, nTop, nBottom):
-	print ("Bottom=%d Top=%d Wall=%d Air=%d" % (nBottom, nTop, nWall, nAir))
-
-
-# Distribucija vremena zivota cestice
-def distrib(x):
-	return (-x*log(random()))
-
-# Gausova distribucija
-def Gauss(particle):
-	if particle.gauss_kon == 0:
-		s1 = random()
-		s2 = random()
-		temp = sqrt(-2 * log(s1))
-		fi = 2 * pi * s2
-		value = temp * cos(fi)
-		particle.gauss_y = temp * sin(fi)
-		particle.gauss_kon = 1
-	else:
-		value = particle.gauss_y
-		particle.gauss_kon = 0
-
-	return value
-
-
-# Brzina po Maxwell raspodeli
-def speed_maxwell(am, te, particle):
-	coeff = sqrt(8.314 * te / am)
-	return [Gauss(particle)*coeff, Gauss(particle)*coeff, Gauss(particle)*coeff]
 
 # Trenutna pozicija cestice
 def in_position(x,y,z):
@@ -60,6 +31,36 @@ def in_position(x,y,z):
 
 	return result
 
+# Stampaj status
+def print_status(nAir, nWall, nTop, nBottom):
+	print ("Bottom=%d Top=%d Wall=%d Air=%d" % (nBottom, nTop, nWall, nAir))
+
+# Distribucija vremena zivota cestice
+def distrib(x):
+	return (-x*log(random()))
+
+# Gausova distribucija
+def Gauss():
+	global gauss_kon
+	global gauss_y
+	if gauss_kon == 0:
+		s1 = random()
+		s2 = random()
+		temp = sqrt(-2 * log(s1))
+		fi = 2 * pi * s2
+		value = temp * cos(fi)
+		gauss_y = temp * sin(fi)
+		gauss_kon = 1
+	else:
+		value = gauss_y
+		gauss_kon = 0
+
+	return value
+
+# Brzina po Maxwell raspodeli
+def speed_maxwell(am, te):
+	coeff = sqrt(8.314 * te / am)
+	return [Gauss()*coeff, Gauss()*coeff, Gauss()*coeff]
 
 def distance(p1, p2):
 	return sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2+(p1.z-p2.z)**2)
@@ -68,19 +69,14 @@ def distance(p1, p2):
 # Klasa za particle
 #
 class Particle:
-	def __init__(self, id):
+	def __init__(self, id, half_life, x, y, z):
 		self.id = id
 		self.life_time = 0.
-		self.half_life = distrib(tau)
+		self.half_life = half_life
 		self.status = INIT
-		self.gauss_kon = 0
-		self.gauss_y = 0
-
-		self.z = 1.e-2 * random()
-		rr = r*sqrt(random())
-		fir = 2*pi*random()
-		self.x = rr*cos(fir)
-		self.y = rr*sin(fir)
+		self.x = x
+		self.y = y
+		self.z = z
 
 	def __str__(self):
 		return str(self.status)
