46665ff11c40f75bb9b824f0d29a5f5ae283b7bc
srdjan034 <snikolic.imi@gmail.com>
2017-07-07 20:01:03 +0200

    RadonCounter se prvi pokrece

--- rabbitMQ/pokreniRabbitMQ.sh
+++ rabbitMQ/pokreniRabbitMQ.sh
@@ -3,6 +3,10 @@
 particles_num=`python procitajJsonPolje.py configuration.json brojCestica`       # ukupan broj cestica
 producers_num=`python procitajJsonPolje.py configuration.json brojProizvodjaca`  # ukupan broj proizvojaca
 
+echo 'Simulacija je u toku...'
+# pokreni program koji sabira sve statuse cestica
+time python RadonCounter.py &
+
 # pokreni skripta koja obradjuju po jednu cesticu
 for id in `seq 1 $particles_num`
 do
@@ -15,8 +19,9 @@ do
     python PathProducer.py &
 done
 
-echo 'Simulacija je u toku...'
-# pokreni program koji sabira sve statuse cestica
-time python RadonCounter.py
+
+
+
+
 
 
