e0e67376ea311ef139b5043bd3d2f69256a68208
srdjan034 <snikolic.imi@gmail.com>
2017-07-10 18:31:39 +0200

    Dodat bounding box

--- rabbitMQ/PathConsumer.py
+++ rabbitMQ/PathConsumer.py
@@ -24,14 +24,79 @@ def uzmiPocetneVrednosti(ch, method, properties, pointsJson):
     # Odjavi se
     channel.stop_consuming()
 
+# funkcija proverava da li bounding box putanje iskace iz cilindra za prosledjene koordinate cestice
+def checkBoundingBox(xParticle, yParticle, zParticle, xMin, yMin, zMin, xMax, yMax, zMax, life_time_step_sum):
+
+    global particle
+
+    x = xParticle + xMax
+    y = yParticle + yMin
+    z = zParticle + zMin
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMax
+    y = yParticle + yMin
+    z = zParticle + zMax
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMin
+    y = yParticle + yMin
+    z = zParticle + zMax
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMin
+    y = yParticle + yMin
+    z = zParticle + zMin
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMax
+    y = yParticle + yMax
+    z = zParticle + zMin
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMax
+    y = yParticle + yMax
+    z = zParticle + zMax
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMin
+    y = yParticle + yMax
+    z = zParticle + zMax
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    x = xParticle + xMin
+    y = yParticle + yMax
+    z = zParticle + zMax
+
+    if in_position(x, y, z) != AIR:
+        return True
+
+    # proveri da li se cestica raspala
+    if (particle.life_time + life_time_step_sum) >= particle.half_life:
+        return True
+
 def obradiPutanju(ch, method, properties, pointsJson):
 
     global particle
     global connection
     global channel
     global particleId
-    message = ""
     global ii
+    message = ""
 
     pointsData = json.loads(pointsJson)
 
@@ -40,50 +105,72 @@ def obradiPutanju(ch, method, properties, pointsJson):
     zList = pointsData['z']
     life_time_step = pointsData['life_time_step']
 
-    for i in range(len(xList)):
-        x = particle.x + xList[i]
-        y = particle.y + yList[i]
-        z = particle.z + zList[i]
+    xMin = pointsData['xMin']
+    xMax = pointsData['xMax']
+    yMin = pointsData['yMin']
+    yMax = pointsData['yMax']
+    zMin = pointsData['zMin']
+    zMax = pointsData['zMax']
 
-        pos_status = in_position(x, y, z)
+    x_last = pointsData['x_last']
+    y_last = pointsData['y_last']
+    z_last = pointsData['z_last']
 
-        if pos_status == AIR:
-            particle.life_time += life_time_step[i]
+    life_time_step_sum = pointsData['life_time_step_sum']
 
-            if particle.life_time >= particle.half_life:
-                message = "AIR"
-            else:
-                particle.set(x, y, z)
-                continue
-        # Cestica se nije raspala
-        else:
-            particle.life_time += life_time_step[i]
+    # proveri da li "bounding box" putanje iskace iz cilindra za trenutne koordinate cestice
+    if checkBoundingBox(particle.x, particle.y, particle.z, xMin, yMin, zMin, xMax, yMax, zMax, life_time_step_sum):
+        for i in range(len(xList)):
+            x = particle.x + xList[i]
+            y = particle.y + yList[i]
+            z = particle.z + zList[i]
 
-            if particle.life_time > particle.half_life:
-                message = "AIR"
+            pos_status = in_position(x, y, z)
 
-            else:
-                if pos_status == BOTTOM:
-                    message = "BOTTOM"
-                elif pos_status == TOP:
-                    message = "TOP"
+            if pos_status == AIR:
+                particle.life_time += life_time_step[i]
+
+                if particle.life_time >= particle.half_life:
+                    message = "AIR"
                 else:
-                    message = "WALL"
+                    particle.set(x, y, z)
+                    continue
+            # Cestica se nije raspala
+            else:
+                particle.life_time += life_time_step[i]
 
+                if particle.life_time > particle.half_life:
+                    message = "AIR"
+
+                else:
+                    if pos_status == BOTTOM:
+                        message = "BOTTOM"
+                    elif pos_status == TOP:
+                        message = "TOP"
+                    else:
+                        message = "WALL"
+            break
+    else:
+        particle.life_time += life_time_step_sum
+        particle.set(particle.x + x_last, particle.y + y_last, particle.z + z_last)
+
+    if message != "":
         channel.queue_declare(queue='particleCounter')
 
         ch.basic_publish(exchange='',
-                              routing_key='particleCounter',
-                              body=message,
-                              properties=pika.BasicProperties(
-                                  delivery_mode=2,  # make message persistent
-                              ))
+                         routing_key='particleCounter',
+                         body=message,
+                         properties=pika.BasicProperties(
+                             delivery_mode=2  # make message persistent
+                         ))
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
         ch.basic_cancel(consumer_tag='radonSim')
         print 'Cestica ' + str(particleId) + ' je zavrsila.'
         print "(" + str(particle.x) + "," + str(particle.y) + "," + str(particle.z) + ")"
         sys.exit(0)
+
+
 
 if __name__ == "__main__":
 
@@ -111,8 +198,8 @@ if __name__ == "__main__":
             channel.connection.process_data_events(time_limit=1)  # 1 second
 
         # Uzimaj putanje
-        args = {'x-max-length': particles_num * 3}
-        channel.queue_declare(queue='path', arguments=args)
+        args = {'x-max-length': 40}
+        channel.queue_declare(queue='path')#, arguments=args)
 
         channel.basic_consume(obradiPutanju,
                                 queue='path')
