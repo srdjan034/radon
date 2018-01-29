efcc44d75a514bc1f5d25813df2a0eeb6f448930
srdjan034 <snikolic.imi@gmail.com>
2018-01-29 13:21:07 +0100

    Inicijalna verzija koja koristi MPI

---
+++ C_mpi/particle.h
@@ -0,0 +1,435 @@
+/* 
+ * File:   particle.h
+ * Author: srdjan
+ *
+ * Created on January 23, 2018, 1:14 PM
+ */
+
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <float.h>
+#include <unistd.h>
+#include <mpi.h> 
+
+// Job parameters
+double r;
+double h;
+double half_path;
+double tau;
+
+
+enum DEPORTATION_PLACE
+{
+	INIT = -1,
+	AIR = 0,
+	WALL = 1,
+	TOP = 2,
+	BOTTOM = 3,
+        DECOMPOSED = 4
+};
+
+typedef struct
+{
+    int id;
+    double life_time;
+    double half_life;
+    enum DEPORTATION_PLACE status;
+
+    double z;
+    double x;
+    double y;
+
+} Particle;
+
+typedef struct 
+{
+    double xMin;
+    double xMax;
+    double yMin;
+    double yMax;
+    double zMin;
+    double zMax;
+    double xLast;
+    double yLast;
+    double zLast;
+    double lifeTimeStepSum;
+    //unsigned int seed;
+    struct drand48_data seed;
+} Partial_trajectory;
+
+void initialize()
+{
+    // Job parameters
+    r = 0.04;
+    h = 0.08;
+    half_path = 6.0e-6;
+    tau = 183/log(2);
+}
+
+/*
+double randomDoubleWithSeed(unsigned int * seed)
+{
+    return (double) rand_r(seed) / RAND_MAX ;
+}
+ */
+
+double randomDoubleWithSeed(struct drand48_data * buffer_seed)
+{
+    double value;
+    drand48_r( buffer_seed, &value);
+    return value;
+}
+
+double distrib(double x, struct drand48_data * buffer_seed)
+{
+	return (-x*log(randomDoubleWithSeed(buffer_seed)));
+}
+
+double Gauss(int * gauss_kon, double * gauss_y, struct drand48_data * buffer_seed)
+{
+    double value;
+    double s1, s2, temp, fi;
+
+    if(*gauss_kon == 0)
+    {
+        s1 = randomDoubleWithSeed(buffer_seed);
+        s2 = randomDoubleWithSeed(buffer_seed);
+        temp = sqrt(-2 * log(s1));
+        fi = 2 * M_PI * s2;
+        value = temp * cos(fi);
+        *gauss_y = temp * sin(fi);
+        *gauss_kon = 1;
+    }
+    else
+    {
+        value = *gauss_y;
+        *gauss_kon = 0;
+    }
+
+    return value;
+}
+
+double * speed_maxwell(double am, double te, double * values, struct drand48_data * buffer_seed, int * gauss_kon, double * gauss_y)
+{	
+	double coeff = sqrt(8.314 * te / am);
+
+	values[0] = Gauss(gauss_kon, gauss_y, buffer_seed)*coeff;
+	values[1] = Gauss(gauss_kon, gauss_y, buffer_seed)*coeff;
+	values[2] = Gauss(gauss_kon, gauss_y, buffer_seed)*coeff;
+
+	return values;
+}
+
+enum DEPORTATION_PLACE in_position(double x, double y, double z)
+{
+    enum DEPORTATION_PLACE result = AIR;
+
+    double xy = sqrt(x*x + y*y);
+
+    if ( xy >= r )
+            result = WALL;
+    else if ( z >= h )
+            result = TOP;
+    else if (z <= 0)
+            result = BOTTOM;
+
+    return result;
+}
+
+double distance(Particle * p1, Particle * p2)
+{
+    return sqrt ( (p1->x - p2->x) * (p1->x - p2->x) + 
+                  (p1->y - p2->y) * (p1->y - p2->y) + 
+                  (p1->z - p2->z) * (p1->z - p2->z)
+                );
+}
+
+void initialize_particle(Particle * particle, int id, double tau, double r, struct drand48_data * buffer_seed)
+{
+	particle->id = id;
+	particle->life_time = 0.0;
+	particle->half_life = distrib(tau, buffer_seed);
+	particle->status = INIT;
+
+	particle->z = 1.e-2 * randomDoubleWithSeed(buffer_seed);
+	double rr = r * sqrt(randomDoubleWithSeed(buffer_seed));
+	double fir = 2 * M_PI * randomDoubleWithSeed(buffer_seed);
+	particle->x = rr * cos(fir);
+	particle->y = rr * sin(fir);
+}
+
+void set(Particle * p, double x, double y, double z)
+{
+    p->x = x;
+    p->y = y;
+    p->z = z;
+}
+
+void print_status(int nAir, int nWall, int nTop, int nBottom)
+{
+    printf("Bottom=%d Top=%d Wall=%d Air=%d\n", nBottom, nTop, nWall, nAir);
+}
+
+int checkDepositionPlace(Particle * particle, int nPathPoints, struct drand48_data * buffer_seed, int * count)
+{
+    enum DEPORTATION_PLACE pos_status;
+    
+    double impactDistance = 0.0;
+    double fi0, theta0;
+    double v_magnitude;
+    double * v = (double * )calloc(3, sizeof(double));
+    double lifeTimeStepSum = 0.0;
+    double xTemp, yTemp, zTemp;
+    int gauss_kon = 1;
+    double gauss_y = 0.0;
+    
+    double xMin = DBL_MAX;
+    double xMax = -DBL_MAX;
+    double yMin = DBL_MAX;
+    double yMax = -DBL_MAX;
+    double zMin = DBL_MAX;
+    double zMax = -DBL_MAX; 
+    
+    int i;
+    for (i = 0; i < nPathPoints; i++) 
+    {
+        (*count)++;
+        
+        impactDistance = distrib(half_path, buffer_seed);
+        
+        fi0 = 2 * M_PI * randomDoubleWithSeed(buffer_seed);
+        theta0 = acos(1 - 2 * randomDoubleWithSeed(buffer_seed));
+        
+        xTemp = particle->x + impactDistance * sin(theta0) * cos(fi0);
+	yTemp = particle->y + impactDistance * sin(theta0) * sin(fi0);
+	zTemp = particle->z + impactDistance * cos(theta0);
+        
+        if(xTemp < xMin) xMin = xTemp;
+	if(yTemp < yMin) yMin = yTemp;
+	if(zTemp < zMin) zMin = zTemp;
+				
+	if(xTemp > xMax) xMax = xTemp;
+	if(yTemp > yMax) yMax = yTemp;
+	if(zTemp > zMax) zMax = zTemp;
+        
+        speed_maxwell(0.2, 293.0, v, buffer_seed, &gauss_kon, &gauss_y);
+        v_magnitude = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+        
+        pos_status = in_position(xTemp,yTemp,zTemp);
+        
+        if(pos_status == AIR)
+	{
+            particle->life_time += (impactDistance / v_magnitude);
+            
+            if (particle->life_time >= particle->half_life)
+            {
+                particle->status = AIR;
+                break;
+            }
+            else
+            {
+                particle->x = xTemp;
+                particle->y = yTemp;
+                particle->z = zTemp;
+                continue;
+            }	
+	}
+	else
+	{
+            particle->life_time += (impactDistance / v_magnitude);
+
+            if (particle->life_time > particle->half_life)
+            {
+                particle->status = AIR;
+            }
+            else
+            {
+                if(pos_status == BOTTOM)
+                {
+                    particle->status = BOTTOM;
+                }
+                else if(pos_status == TOP)
+                {
+                    particle->status = TOP;
+                }
+                else
+                {
+                    particle->status = WALL;
+                }
+            }
+            
+            break;
+	}
+    }
+    
+    if( i == nPathPoints)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
+
+Partial_trajectory * generatePartialTrajectory(int * gauss_kon, double * gauss_y, int nPathPoints, struct drand48_data * buffer_seed)
+{
+    double impactDistance = 0.0;
+    double fi0, theta0;
+    double v_magnitude;
+    double * v = (double * )calloc(3, sizeof(double));
+    double lifeTimeStepSum = 0.0;
+    
+    Partial_trajectory * partial_trajectory = 
+            (Partial_trajectory *)malloc(sizeof(Partial_trajectory));
+    
+    struct drand48_data seedPathStart = * buffer_seed; 
+    
+    double xMin = DBL_MAX;
+    double xMax = -DBL_MAX;
+    double yMin = DBL_MAX;
+    double yMax = -DBL_MAX;
+    double zMin = DBL_MAX;
+    double zMax = -DBL_MAX; 
+    
+    double xLast = .0; 
+    double yLast = .0;
+    double zLast = .0;
+    
+    int i;
+    for (i = 0; i < nPathPoints; i++) 
+    {
+        impactDistance = distrib(half_path, buffer_seed);
+        
+        fi0 = 2 * M_PI * randomDoubleWithSeed(buffer_seed);
+        theta0 = acos(1 - 2 * randomDoubleWithSeed(buffer_seed));
+        
+        xLast += impactDistance * sin(theta0) * cos(fi0);
+	yLast += impactDistance * sin(theta0) * sin(fi0);
+	zLast += impactDistance * cos(theta0);
+        
+	if(xLast < xMin) xMin = xLast;
+	if(yLast < yMin) yMin = yLast;
+	if(zLast < zMin) zMin = zLast;
+				
+	if(xLast > xMax) xMax = xLast;
+	if(yLast > yMax) yMax = yLast;
+	if(zLast > zMax) zMax = zLast;
+        
+        speed_maxwell(0.2, 293.0, v, buffer_seed, gauss_kon, gauss_y);
+        
+        v_magnitude = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+        
+        lifeTimeStepSum += (impactDistance / v_magnitude);
+    }
+    
+    partial_trajectory->xMin = xMin;
+    partial_trajectory->xMax = xMax;
+    partial_trajectory->yMin = yMin;
+    partial_trajectory->yMax = yMax;
+    partial_trajectory->zMin = zMin;
+    partial_trajectory->zMax = zMax;
+    partial_trajectory->xLast = xLast;
+    partial_trajectory->yLast = yLast;
+    partial_trajectory->zLast = zLast;
+    partial_trajectory->lifeTimeStepSum = lifeTimeStepSum;
+    partial_trajectory->seed = seedPathStart;
+    
+    free(v);
+    
+    return  partial_trajectory;
+}
+
+enum DEPORTATION_PLACE checkBoundingBox(Particle * particle, Partial_trajectory * partial_trajectory)
+{
+    enum DEPORTATION_PLACE p = AIR;
+    
+    double x = particle->x + partial_trajectory->xMax;
+    double y = particle->y + partial_trajectory->yMin;
+    double z = particle->z + partial_trajectory->zMin;
+
+    if( (p = in_position(x, y, z)) != AIR)
+    {
+        return p;
+    }
+
+    x = particle->x + partial_trajectory->xMax;
+    y = particle->y + partial_trajectory->yMin;
+    z = particle->z + partial_trajectory->zMax;
+
+    if( (p = in_position(x, y, z)) != AIR)
+    {
+        return p;
+    }
+
+    x = particle->x + partial_trajectory->xMin;
+    y = particle->y + partial_trajectory->yMin;
+    z = particle->z + partial_trajectory->zMax;
+
+    if( (p = in_position(x, y, z)) != AIR)
+    {
+        return p;
+    }
+
+    x = particle->x + partial_trajectory->xMin;
+    y = particle->y + partial_trajectory->yMin;
+    z = particle->z + partial_trajectory->zMin;
+
+    if( (p = in_position(x, y, z)) != AIR)
+    {
+        return p;
+    }
+
+   x = particle->x + partial_trajectory->xMax;
+   y = particle->y + partial_trajectory->yMax;
+   z = particle->z + partial_trajectory->zMin;
+
+   if( (p = in_position(x, y, z)) != AIR)
+   {
+       return p;
+   }
+
+   x = particle->x + partial_trajectory->xMax;
+   y = particle->y + partial_trajectory->yMax;
+   z = particle->z + partial_trajectory->zMax;
+
+   if( (p = in_position(x, y, z)) != AIR)
+   {
+       return p;
+   }
+
+   x = particle->x + partial_trajectory->xMin;
+   y = particle->y + partial_trajectory->yMax;
+   z = particle->z + partial_trajectory->zMax;
+
+   if( (p = in_position(x, y, z)) != AIR)
+   {
+       return p;
+   }
+
+   x = particle->x + partial_trajectory->xMin;
+   y = particle->y + partial_trajectory->yMax;
+   z = particle->z + partial_trajectory->zMin;
+
+   if( (p = in_position(x, y, z)) != AIR)
+   {
+       return p;
+   }
+
+   if((particle->life_time + partial_trajectory->lifeTimeStepSum) >= particle->half_life)
+   {
+       return DECOMPOSED;
+   }
+   
+   return p;
+}
+
+void * waitForSignal(void * t)
+{
+    int n;
+    MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    
+    int * running = (int * ) t;
+    *running = 0;
+}
---
+++ C_mpi/radon_mpi.c
@@ -0,0 +1,182 @@
+/* 
+ * 
+ * File:   radon_mpi.c
+ * Author: srdjan
+ *
+ * Created on January 23, 2018, 1:06 PM
+ * 
+ */
+
+#include "particle.h"
+
+void main(int argc, char *argv[]) 
+{
+    // Particles
+    Particle * particles;
+    int particles_num = 1024; 
+    int nPathPoints = 10000;
+    int gauss_kon = 1;
+    double gauss_y = 0.0;
+    
+    // mpi
+    int rank, size; 
+    double param; 
+    MPI_Datatype dt_point;
+
+    int running = 1;
+
+    MPI_Init(&argc, &argv); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    MPI_Request requests[size];
+    
+    initialize();
+    
+    struct drand48_data buffer_seed;
+    srand48_r(rank, &buffer_seed);
+
+    MPI_Type_contiguous(11, MPI_DOUBLE, &dt_point);
+    MPI_Type_commit(&dt_point);
+
+    Partial_trajectory partial_trajectory;
+
+    if(rank == 0)
+    {   
+        /*
+            Particles
+         */
+        int i, j;
+        
+        int nAir, nWall, nTop, nBottom;
+        nAir = nWall = nTop = nBottom = 0;
+        int nDeportedParticles = 0;
+        
+        Partial_trajectory * partial_trajectory;
+        particles = (Particle *)malloc(particles_num * sizeof(Particle));
+        for(i = 0; i < particles_num; i++)
+            initialize_particle(&particles[i], i, tau, r, &buffer_seed);
+        
+        Particle * particle = &particles[nDeportedParticles];
+        
+        enum DEPORTATION_PLACE p;
+        int count = 0;
+        
+        FILE * f = fopen("result_mpi.csv", "w");
+        clock_t begin = clock();
+        
+        /*
+            MPI
+         */
+        MPI_Request array_of_requests[size - 1];
+        int outcount;
+        int array_of_indices[size - 1];
+        MPI_Status array_of_statuses[size - 1];
+        Partial_trajectory buffer[100];
+        
+        // Start a standard-mode, nonblocking receive. 
+        for(i = 1; i < size; i++)
+        {   
+            MPI_Irecv(&buffer[i-1], 1, dt_point, i, 0, MPI_COMM_WORLD, &requests[i-1]);
+        }
+        
+        while(nDeportedParticles != particles_num)
+        {
+            // Tests for some given requests to complete
+            MPI_Testsome(size-1, requests, &outcount, array_of_indices, array_of_statuses); 
+            
+            // For every completed requests
+            for(i = 0; i < outcount; i++)
+            {
+                partial_trajectory = &buffer[array_of_indices[i]];
+                
+                if( (p = checkBoundingBox(particle, partial_trajectory)) != AIR)
+                {
+                    
+                    if(checkDepositionPlace(particle, nPathPoints, &partial_trajectory->seed, &count) == 1)
+                    {
+                        fprintf(f, "%d, %d, %lf\n", particle->id, count, (double)(clock() - begin) / CLOCKS_PER_SEC);
+                        fflush(f);
+
+                        nDeportedParticles++;
+
+                        switch(particle->status)
+                        {
+                            case TOP:
+                                nTop++;
+                                break;
+                            case BOTTOM:
+                                nBottom++;
+                                break;
+                            case WALL:
+                                nWall++;
+                                break;
+                            case AIR:
+                                nAir++;
+                                break;
+                        }
+
+                        print_status(nAir, nWall, nTop, nBottom);
+
+                        if(nDeportedParticles == particles_num)
+                            break;	
+                        else
+                        {
+                            particle = &particles[nDeportedParticles];
+                            count = 0;
+                        }
+                    }
+                }
+                else
+                {		
+                    particle->life_time += partial_trajectory->lifeTimeStepSum;
+                    particle->x += partial_trajectory->xLast;
+                    particle->y += partial_trajectory->yLast;
+                    particle->z += partial_trajectory->zLast;
+                    
+                    count += nPathPoints;
+                }
+                
+                MPI_Irecv(&buffer[array_of_indices[i]], 1, dt_point, array_of_indices[i]+1, 0, MPI_COMM_WORLD, &requests[array_of_indices[i]]);
+            }
+        }
+        
+        print_status(nAir, nWall, nTop, nBottom);
+        fprintf(f, "nAir = %d, nWall = %d, nTop = %d, nBottom = %d \n", nAir, nWall, nTop, nBottom);
+         
+        fclose(f);
+        
+        // Posalji poruke za kraj
+        int n = 1;
+        for(i=1; i < size; i++)
+            MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        
+    }
+    else
+    {
+        
+        /*
+            Wait for message to end in separate thread.
+            Thread will set "running" to 0.
+         */
+        pthread_t thread;
+        pthread_attr_t attr;
+    
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+        
+        pthread_create(&thread, &attr, waitForSignal, (void *) & running);
+        
+        /*
+            Generate partial trajectories
+         */
+        while(running)
+        {
+            Partial_trajectory * partial_trajectory = 
+                    generatePartialTrajectory(&gauss_kon, &gauss_y, nPathPoints, &buffer_seed);
+            MPI_Send(partial_trajectory, 1, dt_point, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    MPI_Finalize(); 
+}
